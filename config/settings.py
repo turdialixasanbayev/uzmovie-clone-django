07cafe27e733be3586a3a66fb93efe9e58797ab2
Turdiali <turdialixasanbayev@gmail.com>
2025-09-12 21:40:16 +0500

    `Updated Django model migrations with help texts and indexes`

--- config/settings.py
+++ config/settings.py
@@ -140,27 +140,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 ### psql
 
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_name',
-        'USER': 'db_user',
-        'PASSWORD': 'db_user_password',
+        'NAME': 'uzmovie',
+        'USER': 'uzmovie',
+        'PASSWORD': 'uzmovie',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
-"""
 
 
 # Password validation
